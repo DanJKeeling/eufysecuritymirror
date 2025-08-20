e8e41c2950b03a5daaad8fbe0b9069814d5166f8
DanJKeeling <58738849+DanJKeeling@users.noreply.github.com>
2025-08-20 14:25:24 +0100

    Update config_flow.py

--- custom_components/eufy_security/config_flow.py
+++ custom_components/eufy_security/config_flow.py
@@ -8,10 +8,11 @@ from homeassistant.config_entries import SOURCE_REAUTH, ConfigEntry
 from homeassistant.core import callback
 from homeassistant.helpers import aiohttp_client
 import homeassistant.helpers.config_validation as cv
+from homeassistant.helpers.event import async_call_later
 
 from .const import COORDINATOR, DOMAIN
 from .eufy_security_api.api_client import ApiClient
-from .eufy_security_api.exceptions import WebSocketConnectionError
+from .eufy_security_api.exceptions import WebSocketConnectionException
 from .model import Config, ConfigField
 
 _LOGGER = logging.getLogger(__name__)
@@ -24,7 +25,7 @@ class EufySecurityOptionFlowHandler(config_entries.OptionsFlow):
         """Initialize option flow handler"""
         self.config = Config.parse(config_entry)
         self.config_entry = config_entry
-        _LOGGER.debug(f"{DOMAIN} EufySecurityOptionFlowHandler - {config_entry.data}")
+        _LOGGER.debug(f"{DOMAIN} EufySecurityOptionFlowHandler - {config_entry.options}")
         self.schema = vol.Schema(
             {
                 vol.Optional(ConfigField.sync_interval.name, default=self.config.sync_interval): int,
@@ -57,7 +58,7 @@ class EufySecurityFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
         _LOGGER.debug(f"{DOMAIN} EufySecurityOptionFlowHandler - {config_entry.data}")
         return EufySecurityOptionFlowHandler(config_entry)
 
-    def __init__(self):
+    def __init__(self) -> None:
         self._errors = {}
 
     async def async_step_user(self, user_input=None):
@@ -76,8 +77,20 @@ class EufySecurityFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
                 coordinator.config.captcha_img = None
                 await coordinator.set_captcha_and_connect(captcha_id, captcha_input)
 
-            if self._async_current_entries():
-                await self.hass.config_entries.async_reload(self.context["entry_id"])
+            config_entry_id = None
+            for entry in self._async_current_entries():
+                config_entry_id = entry.entry_id
+
+            async def try_reloading(_now):
+                _LOGGER.debug(f"{DOMAIN} try_reloading start after captcha/mfa")
+                await coordinator.disconnect()
+                self.hass.data[DOMAIN] = {}
+                await self.hass.config_entries.async_reload(config_entry_id)
+                _LOGGER.debug(f"{DOMAIN} try_reloading finish after captcha/mfa")
+
+            async_call_later(self.hass, 3, try_reloading)
+            return self.async_abort(reason="reauth_successful")
+
 
         if self._async_current_entries():
             return self.async_abort(reason="single_instance_allowed")
@@ -106,11 +119,11 @@ class EufySecurityFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
     async def _test_credentials(self, host, port):  # pylint: disable=unused-argument
         try:
             config = Config(host=host, port=port)
-            api_client: ApiClient = ApiClient(config, aiohttp_client.async_get_clientsession(self.hass))
+            api_client: ApiClient = ApiClient(config, aiohttp_client.async_get_clientsession(self.hass), None)
             await api_client.ws_connect()
             await api_client.disconnect()
             return True
-        except WebSocketConnectionError as ex:  # pylint: disable=broad-except
+        except WebSocketConnectionException as ex:  # pylint: disable=broad-except
             _LOGGER.error(f"{DOMAIN} Exception in login : %s - traceback: %s", ex, traceback.format_exc())
         return False
 
@@ -132,6 +145,7 @@ class EufySecurityFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
                             vol.Required(ConfigField.mfa_input.name): str,
                         }
                     ),
+                    description_placeholders={"captcha_img": 'Enter Multi Factor Authentication Code'},
                 )
             else:
                 return self.async_show_form(
@@ -141,9 +155,6 @@ class EufySecurityFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
                             vol.Required(ConfigField.captcha_input.name): str,
                         }
                     ),
-                    description_placeholders={
-                        "captcha_img": '<img id="eufy_security_captcha" src="' + coordinator.config.captcha_img + '"/>'
-                    },
+                    description_placeholders={"captcha_img": '<img id="eufy_security_captcha" src="' + coordinator.config.captcha_img + '"/>'},
                 )
         return await self.async_step_user(user_input)
-
